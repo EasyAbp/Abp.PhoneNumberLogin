0259028fa7ede21d35c73f9f9ff0ea2865271aef
gdlcf88 <gdlcf88@outlook.com>
2023-03-23 13:03:53 +0800

    Upgrade to ABP 7.1.0

--- samples/PhoneNumberLoginSample/src/PhoneNumberLoginSample.EntityFrameworkCore/Migrations/20230323050240_UpgradedToAbp_7_1.Designer.cs
+++ samples/PhoneNumberLoginSample/src/PhoneNumberLoginSample.EntityFrameworkCore/Migrations/20230323050240_UpgradedToAbp_7_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhoneNumberLoginSample.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace PhoneNumberLoginSample.Migrations
 {
     [DbContext(typeof(PhoneNumberLoginSampleDbContext))]
-    partial class PhoneNumberLoginSampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230323050240_UpgradedToAbp_7_1")]
+    partial class UpgradedToAbp71
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
