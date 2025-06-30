63b40b0153d502c6212f43ade550280f78b31714
gdlcf88 <gdlcf88@outlook.com>
2025-06-30 19:18:08 +0800

    Add missing EF migrations

--- samples/PhoneNumberLoginSample/src/PhoneNumberLoginSample.EntityFrameworkCore/Migrations/20250630111724_Upgraded_To_Abp_9_2.Designer.cs
+++ samples/PhoneNumberLoginSample/src/PhoneNumberLoginSample.EntityFrameworkCore/Migrations/20250630111724_Upgraded_To_Abp_9_2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhoneNumberLoginSample.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace PhoneNumberLoginSample.Migrations
 {
     [DbContext(typeof(PhoneNumberLoginSampleDbContext))]
-    partial class PhoneNumberLoginSampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250630111724_Upgraded_To_Abp_9_2")]
+    partial class Upgraded_To_Abp_9_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
